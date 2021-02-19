850e7d84d095dfd920039790fb681ab553fba938
Marat-b <buran17@mail.ru>
2021-02-19 11:18:17 +0600

    Added class BaseEnquiry, updated class Enquiry

--- data/enquiry.py
+++ data/enquiry.py
@@ -1,15 +1,14 @@
 import requests
 import json
 from data import config, access_id
+from data.base_enquiry import BaseEnquiry
 
 
-class Enquiry:
+class Enquiry(BaseEnquiry):
 	
 	def __init__(self, telegram_user_id):
-		self.__url_read_table = config.URL_READ_TABLE
-		self.__request_telegramuser = config.REQUEST_TABLE_TELEGRAMUSER
-		self.__request_telegramuser["filter"]["row"]["f81371"]["value"] = str(telegram_user_id)
-		self.__user_id = self.__get_user_id(telegram_user_id)
+		super().__init__(telegram_user_id)
+		self.__user_id = self._get_user_id(telegram_user_id)
 	
 	def get_entities(self, type_request):
 		"""
@@ -21,71 +20,8 @@ class Enquiry:
 		request_db = config.REQUEST_DB.copy()
 		request_db["filter"]["row"]["f79831"]["value"] = self.__user_id
 		request_db["filter"]["row"]["f78321"]["value"] = type_request
-		all_records = self.__get_records(request_db)
+		all_records = self._get_records(request_db)
 		return all_records
-	
-	def __get_records(self, request_db):
-		"""
-		Get records of clients once
-		:param: db's records
-		:return: All records of clients
-		"""
-		all_clients = list()
-		id_access = access_id.Auth.get_access_id()
-		if access_id is None:
-			return all_clients
-		request_db["access_id"] = id_access
-		clients, json_table = self.__get_all_records(request_db, 0, int(config.MAX_AMOUNT_RECORDS))
-		# summary all clients
-		all_clients += clients
-		amount_returned_records = int(json_table['count'])
-		# print(f"returned_records = {amount_returned_records}")
-		amount_all_records = int(json_table['count_all'])
-		if amount_all_records > amount_returned_records:
-			clients, _ = self.__get_all_records(request_db, amount_returned_records, amount_all_records)
-			all_clients += clients
-		return all_clients
-	
-	def __get_all_records(self, request_db, amount_returned_records, amount_all_records):
-		"""
-		Get added records of clients if amount_all_records greater than amount_returned_records
-		:param request_db:
-		:param amount_returned_records:
-		:param amount_all_records:
-		:return: All records of clients
-		"""
-		clients = list()
-		request_db["start"] = amount_returned_records
-		request_db["limit"] = amount_all_records
-		try:
-			# print(f"self.__url_read_table = {self.__url_read_table}, request_db = {request_db}")
-			response_read_table = requests.post(self.__url_read_table, json = request_db)
-			# convert records from text to json
-			# print(f"response_read_table = {response_read_table}, response_read_table.text = {response_read_table.text}")
-			json_table = json.loads(response_read_table.text)
-			if int(json_table["code"]) != 0 or not ("data" in json_table.keys()):
-				return clients, {"count": 0, "count_all": 0}
-			# get data's keys from json with key "data"
-			records = [record for record in json_table['data']]
-			# get records with key "row"
-			clients = [json_table['data'][record]["row"] for record in records]
-			return clients, json_table
-		except requests.exceptions.ConnectionError:
-			return list(), {"count": 0, "count_all": 0}
-	
-	def __get_user_id(self, telegram_user_id):
-		"""
-		Get user ID for Kartpay
-		:return: user ID for Kartpay
-		"""
-		all_records = self.__get_records(self.__request_telegramuser)
-		# print(f"all_records = {all_records}")
-		id_user = None
-		for record in all_records:
-			if str(telegram_user_id) == record["f81371"]:
-				id_user = record["f81361"]
-				break
-		return id_user
 	
 	def update_act(self, **kwargs):
 		"""
@@ -114,7 +50,6 @@ class Enquiry:
 			return True
 		else:
 			return False
-		# return True
 
 
 if __name__ == "__main__":

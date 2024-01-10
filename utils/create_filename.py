3f8619bd063df4430949e559c553401ac4b3e4cd
marat-b <buran17@mail.ru>
2024-01-10 12:41:02 +0500

    TODO

--- utils/create_filename.py
+++ utils/create_filename.py
@@ -1,9 +1,15 @@
 import datetime
 
 
-def create_filename(file_name: str, id_task: str, year: str = str(datetime.datetime.today().year), month: str = str(datetime.datetime.today().month), day: str = str(
-		datetime.datetime.today().day)) -> str:
-	month = month if len(month) == 2 else "0{}".format(month)
-	day = day if len(day) == 2 else "0{}".format(day)
-	template_file = "{}_{}{}{}_{}.jpg".format(file_name, year, month, day, id_task)
-	return template_file
+# TODO  datetime.datetime.today takes date from docker, but it always equal docker started date
+def create_filename(
+    file_name: str,
+    id_task: str,
+    year: str = str(datetime.datetime.today().year),
+    month: str = str(datetime.datetime.today().month),
+    day: str = str(datetime.datetime.today().day),
+) -> str:
+    month = month if len(month) == 2 else "0{}".format(month)
+    day = day if len(day) == 2 else "0{}".format(day)
+    template_file = "{}_{}{}{}_{}.jpg".format(file_name, year, month, day, id_task)
+    return template_file
